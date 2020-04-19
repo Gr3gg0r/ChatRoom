1f9bb6eadbcab54a500de56bd59c9bd39aed56bd
Gr3gg0r <48479663+Gr3gg0r@users.noreply.github.com>
2020-04-19 21:39:46 +0800

    First Ver

---
+++ Client.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.net.*;
+public class Client {
+public static void main(String[] args)
+{
+try {
+Socket sock = new Socket("192.168.182.136",3000);
+SendThread sendThread = new SendThread(sock);
+Thread thread = new Thread(sendThread);thread.start();
+RecieveThread recieveThread = new RecieveThread(sock);
+Thread thread2 =new Thread(recieveThread);thread2.start();
+} catch (Exception e) {System.out.println(e.getMessage());} 
+}
+}
+class RecieveThread implements Runnable
+{
+Socket sock=null;
+BufferedReader recieve=null;
+public RecieveThread(Socket sock) {
+this.sock = sock;
+}
+public void run() {
+try{
+recieve = new BufferedReader(new InputStreamReader(this.sock.getInputStream()));
+String msgRecieved = null;
+while((msgRecieved = recieve.readLine())!= null)
+{
+System.out.println("From Server: " + msgRecieved);
+System.out.println("Please enter something to send to server..");
+}
+}catch(Exception e){System.out.println(e.getMessage());}
+}//end run
+}//end class re
+class SendThread implements Runnable
+{
+Socket sock=null;
+PrintWriter print=null;
+BufferedReader brinput=null;
+
+public SendThread(Socket sock)
+{
+this.sock = sock;
+}//end constructor
+public void run(){
+try{
+if(sock.isConnected())
+{
+System.out.println("Enter Username :");
+this.print = new PrintWriter(sock.getOutputStream(), true);	
+while(true){
+System.out.println("Type your  'EXIT' to exit");
+brinput = new BufferedReader(new InputStreamReader(System.in));
+String msgtoServerString=null;
+msgtoServerString = brinput.readLine();
+this.print.println(msgtoServerString);
+this.print.flush();
+	
+if(msgtoServerString.equals("EXIT"))
+break;
+}//end while
+sock.close();}}catch(Exception e){System.out.println(e.getMessage());}
+}//end run method
+}//end class
